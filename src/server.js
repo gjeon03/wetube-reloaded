269826fe463aea173c5fe403c1f39cbd7b7d9383
JeonGyeongYeon <gjeon0321@gmail.com>
2021-09-26 09:45:09 +0900

    js & node & pug set

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
